972c4ac123480610854a3c4aad87878dd3fdc2ca
clarksl <clark_sl@hughes.net>
2015-10-24 22:55:38 -0500

    Updated to switch to UserDetails class
    Changed Bank Account.cpp to remove 'details' (non-descriptive account) and added UserDetails.h and the UserDetail class.

--- Bank Account/Bank Account.cpp
+++ Bank Account/Bank Account.cpp
@@ -7,40 +7,16 @@
 #include <stdio.h>
 #include "stdafx.h"
 #include <conio.h>
+#include <UserDetails.h>
 
 using namespace std;
-class details
-{
-public:
-	char *name;
-	int age;
-	int accno;
-	char branch[50];
-	char city[40];
-	void getdetails()
-	{
-		name = new char[40];
-		cout << endl << endl << "**********Customer Details*********** " << endl;
-		cout << "          -------- -------            " << endl;
-		cout << "Enter Name: ";
-		std::cin >> name;
-		cout << "Enter Age: ";
-		cin >> age;
-		cout << "Enter Account Number: ";
-		cin >> accno;
-		cout << "Enter Branch: ";
-		cin >> branch;
-		cout << "Enter City: ";
-		cin >> city;
-		cout << "______________________________________" << endl;
-	}
-};
+
 class bank
 {
 public:
 	static int accnumber;
 	long balance;
-	details d;
+	UserDetails d;
 	void getdata();
 	bank transfermoney(bank);
 	void deposit();
