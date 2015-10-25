77d391de738c1e6ee1201d5b40734de3e520a320
clarksl <clark_sl@hughes.net>
2015-10-24 20:19:09 -0500

    Hello
    Testing with a Hello World

---
+++ HelloWorld.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<fstream>
+#include<cctype>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+    system("cls");
+    cout<<"\n\n\n\tHello World!";
+    return 0;
+}
