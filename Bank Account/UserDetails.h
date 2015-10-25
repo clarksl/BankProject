142804c7038509d66d200d01c7cb5e2279f6354b
clarksl <clark_sl@hughes.net>
2015-10-24 22:28:08 -0500

    UserDetails.h
    Header file for the UserDetails class (UserDetails.cpp).

---
+++ Bank Account/UserDetails.h
@@ -0,0 +1,16 @@
+using namespace std;
+/* class definition for UserDetails
+   See UserDetails.cpp for the code for the functions.
+ */
+class UserDetails
+{
+public:
+	char *name;
+	int age;
+	int accno;
+	char branch[50];
+	char city[40];
+	void getdetails();
+	int saveUser();
+	
+};
